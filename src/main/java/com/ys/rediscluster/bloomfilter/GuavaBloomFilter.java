a6487bae83a978663369787d47a62d1acf34c22b
yushuai <shuaiyu@iflytek.com>
2020-05-03 20:55:47 +0800

    Guava BloomFilter

--- src/main/java/com/ys/rediscluster/bloomfilter/GuavaBloomFilter.java
+++ src/main/java/com/ys/rediscluster/bloomfilter/GuavaBloomFilter.java
@@ -1,4 +1,17 @@
 package com.ys.rediscluster.bloomfilter;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnel;
+import com.google.common.hash.Funnels;
+
 public class GuavaBloomFilter {
+    public static void main(String[] args) {
+        BloomFilter<String> bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8),100000,0.01);
+
+        bloomFilter.put("10086");
+
+        System.out.println(bloomFilter.mightContain("123456"));
+        System.out.println(bloomFilter.mightContain("10086"));
+    }
 }
