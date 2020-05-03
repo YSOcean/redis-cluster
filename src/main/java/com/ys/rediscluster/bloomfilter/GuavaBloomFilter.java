d011efc2b37fa2001a7642432c02ceb91fa1c45e
yushuai <shuaiyu@iflytek.com>
2020-05-03 20:38:50 +0800

    redisson 实现布隆过滤器

---
+++ src/main/java/com/ys/rediscluster/bloomfilter/GuavaBloomFilter.java
@@ -0,0 +1,4 @@
+package com.ys.rediscluster.bloomfilter;
+
+public class GuavaBloomFilter {
+}
---
+++ src/main/java/com/ys/rediscluster/bloomfilter/redisson/RedissonBloomFilter.java
@@ -0,0 +1,27 @@
+package com.ys.rediscluster.bloomfilter.redisson;
+
+import org.redisson.Redisson;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+public class RedissonBloomFilter {
+
+    public static void main(String[] args) {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.14.104:6379");
+        config.useSingleServer().setPassword("123");
+        //构造Redisson
+        RedissonClient redisson = Redisson.create(config);
+
+        RBloomFilter<String> bloomFilter = redisson.getBloomFilter("phoneList");
+        //初始化布隆过滤器：预计元素为100000000L,误差率为3%
+        bloomFilter.tryInit(100000000L,0.03);
+        //将号码10086插入到布隆过滤器中
+        bloomFilter.add("10086");
+
+        //判断下面号码是否在布隆过滤器中
+        System.out.println(bloomFilter.contains("123456"));//false
+        System.out.println(bloomFilter.contains("10086"));//true
+    }
+}
